dca92fa92cf84babf4d33474efb9fd4d32e97dd0
Ivan <ivan.fadhila70@gmail.com>
2019-10-29 11:13:50 +0700

    Observable Operator Zip, Merge, Concat

--- app/src/main/java/com/example/newsportal/view/newslist/NewsMVPModel.kt
+++ app/src/main/java/com/example/newsportal/view/newslist/NewsMVPModel.kt
@@ -1,6 +1,7 @@
 package com.example.newsportal.view.newslist
 
 import android.util.Log
+import com.example.newsportal.model.ArticlesItem
 import com.example.newsportal.model.ArticlesResponse
 import com.example.newsportal.model.Source
 import com.example.newsportal.network.ApiClient
@@ -10,8 +11,10 @@ import io.reactivex.Observer
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
+import io.reactivex.functions.BiFunction
 import io.reactivex.observers.DisposableSingleObserver
 import io.reactivex.schedulers.Schedulers
+import java.util.ArrayList
 
 class NewsMVPModel : NewsMVPContract.Model {
     lateinit var apiInterface: ApiInterface
@@ -28,6 +31,44 @@ class NewsMVPModel : NewsMVPContract.Model {
 
         val articleResponseUK = apiInterface.newsUK()
 
+ /*       Observable.zip(
+                articleResponseUK, articlesResponseSingle,
+                //High order function
+                BiFunction <ArticlesResponse, ArticlesResponse, ArticlesResponse>  { articlesResponse, articlesResponse2 ->
+                    //articleResponse UK, articleResponse2 INA
+                    val result = ArticlesResponse()
+                    result.totalResults = articlesResponse.totalResults + articlesResponse2.totalResults
+                    val articlesItems = ArrayList<ArticlesItem>()
+                    articlesItems.addAll(articlesResponse.articles!!)
+                    articlesItems.addAll(articlesResponse2.articles!!)
+                    result.articles = articlesItems
+                    result
+                })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(object : Observer<ArticlesResponse> {
+                    override fun onSubscribe(d: Disposable) {
+
+                    }
+
+                    override fun onNext(articlesResponse: ArticlesResponse) {
+                        val articlesItems = articlesResponse.articles
+                        onFinishedListener.onFinished(articlesItems!!)
+                        //Log.e("newsMerge", articlesItems.toString());
+
+                    }
+
+                    override fun onError(e: Throwable) {
+                        e.message?.let { onFinishedListener.onFailure(it) }
+
+                    }
+
+                    override fun onComplete() {
+
+                    }
+                })
+
+*/
         Observable.concat(articlesResponseSingle, articleResponseUK)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
@@ -53,6 +94,33 @@ class NewsMVPModel : NewsMVPContract.Model {
 
                     }
                 })
+
+/*        Observable.merge(articlesResponseSingle, articleResponseUK)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(object : Observer<ArticlesResponse> {
+                    override fun onSubscribe(d: Disposable) {
+
+                    }
+
+                    override fun onNext(articlesResponse: ArticlesResponse) {
+                        val articlesItems = articlesResponse.articles
+                        onFinishedListener.onFinished(articlesItems!!)
+                        Log.e("newsMerge", articlesItems.toString())
+
+
+                    }
+
+                    override fun onError(e: Throwable) {
+                        e.message?.let { onFinishedListener.onFailure(it) }
+
+                    }
+
+
+                    override fun onComplete() {
+
+                    }
+                })*/
 
     }
 }
